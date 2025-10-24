0ec8347c1f0997788bf3d9a6795cfd5f4b606139
louisharding <lgharding0@gmail.com>
2025-10-24 17:45:42 +0100

    fix(settings): retrieve SECRET_KEY from environment variable

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k%_x(t-!3&8(tulez!8#&p%&v=8844_$sk&4su6vsi7*yfjowu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
