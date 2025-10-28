b2c613320216f9e9fd6138aa361fe1f18007ec77
louisharding <lgharding0@gmail.com>
2025-10-28 17:38:07 +0000

    update(settings): format ALLOWED_HOSTS for consistency

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,10 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '.herokuapp.com',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
